ac5ff5f9b000e554dd6ffa0ca78a9f8b478c43ad
Hamza Abbad <hamza.abbad@gmail.com>
2017-02-01 21:18:06 +0100

    Ajouter le code Arduino initial

---
+++ Code/Arduino/src/CarControl.cpp
@@ -0,0 +1,227 @@
+/*
+Arduino program allowing to control the body of the car and get the readings
+from the distance sensors and the digital temperature and humidity sensor.
+
+Hamza Abbad
+*/
+// #define DEBUG
+#include "CarControl.h"
+
+dht DHTSensor;
+double * distances;
+bool stopped = true;
+DISTANCE_SENSOR direction;
+int speed = 255;
+
+void setup() {
+  // Distance sensors
+  pinMode(DISTANCE_SENSOR_1_TRIG, OUTPUT);
+  pinMode(DISTANCE_SENSOR_1_ECHO, INPUT);
+  pinMode(DISTANCE_SENSOR_2_TRIG, OUTPUT);
+  pinMode(DISTANCE_SENSOR_2_ECHO, INPUT);
+  pinMode(DISTANCE_SENSOR_3_TRIG, OUTPUT);
+  pinMode(DISTANCE_SENSOR_3_ECHO, INPUT);
+  // DC Motors
+  pinMode(RIGHT_MOTOR_PIN1, OUTPUT);
+  pinMode(RIGHT_MOTOR_PIN2, OUTPUT);
+  pinMode(LEFT_MOTOR_PIN1, OUTPUT);
+  pinMode(LEFT_MOTOR_PIN2, OUTPUT);
+  pinMode(RIGHT_MOTOR_SPEED_PIN, OUTPUT);
+  pinMode(LEFT_MOTOR_SPEED_PIN, OUTPUT);
+  // DHT22
+  // pinMode(DHT_DATA, INPUT);
+  DHTSensor.read22(DHT_DATA);
+  // Serial connection
+  Serial.begin(BAUD_RATE);
+  Serial.setTimeout(SERIAL_TIMEOUT);
+  // Random number generation. This pin is not connected
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  if (Serial.available()) {
+    char command = Serial.read();
+    switch (command) {
+      case GET_DISTANCES:
+        distances = getDistances();
+        Serial.print("|");
+        for (size_t i = 0; i < 3; i++) {
+          Serial.print(distances[i]);
+          Serial.print("|");
+        }
+        Serial.println();
+        break;
+      case MOVE_FORWARD:
+        moveForward(speed);
+        delay(MOVE_ACTION_TIME*2);
+        stop();
+        break;
+      case MOVE_BACKWARD:
+        moveBackward(speed);
+        delay(MOVE_ACTION_TIME*2);
+        stop();
+        break;
+      case TURN_RIGHT:
+        turnRight(speed);
+        delay(MOVE_ACTION_TIME*2);
+        stop();
+        break;
+      case TURN_LEFT:
+        turnLeft(speed);
+        delay(MOVE_ACTION_TIME*2);
+        stop();
+        break;
+      case STOP:
+        stop();
+        break;
+      case TEMP:
+        DHTSensor.read22(DHT_DATA);
+        Serial.print("Temp = ");
+        Serial.println(DHTSensor.temperature);
+        break;
+      case SET_SPEED:
+        speed = random(256);
+        Serial.print("Speed = ");
+        Serial.println(speed);
+        break;
+      case NAVIGATE:
+        stopped = false;
+        break;
+    }
+  } else if (!stopped) {
+    navigate(speed);
+    delay(MOVE_ACTION_TIME);
+  }
+}
+
+void forwardRight(byte speed) {
+  digitalWrite(RIGHT_MOTOR_PIN1, HIGH);
+  digitalWrite(RIGHT_MOTOR_PIN2, LOW);
+  analogWrite(RIGHT_MOTOR_SPEED_PIN, speed);
+}
+
+void backwardRight(byte speed) {
+  digitalWrite(RIGHT_MOTOR_PIN1, LOW);
+  digitalWrite(RIGHT_MOTOR_PIN2, HIGH);
+  analogWrite(RIGHT_MOTOR_SPEED_PIN, speed);
+}
+
+void forwardLeft(byte speed) {
+  digitalWrite(LEFT_MOTOR_PIN1, HIGH);
+  digitalWrite(LEFT_MOTOR_PIN2, LOW);
+  analogWrite(LEFT_MOTOR_SPEED_PIN, speed);
+}
+
+void backwardLeft(byte speed) {
+  digitalWrite(LEFT_MOTOR_PIN1, LOW);
+  digitalWrite(LEFT_MOTOR_PIN2, HIGH);
+  analogWrite(LEFT_MOTOR_SPEED_PIN, speed);
+}
+
+void stop() {
+  analogWrite(LEFT_MOTOR_SPEED_PIN, 0);
+  analogWrite(RIGHT_MOTOR_SPEED_PIN, 0);
+  digitalWrite(RIGHT_MOTOR_PIN1, LOW);
+  digitalWrite(RIGHT_MOTOR_PIN2, LOW);
+  digitalWrite(LEFT_MOTOR_PIN1, LOW);
+  digitalWrite(LEFT_MOTOR_PIN2, LOW);
+  stopped = true;
+}
+
+void moveForward(byte speed) {
+  stopped = false;
+  forwardLeft(speed);
+  forwardRight(speed);
+}
+
+void moveBackward(byte speed) {
+  stopped = false;
+  backwardLeft(speed);
+  backwardRight(speed);
+}
+
+void turnRight(byte speed) {
+  stopped = false;
+  backwardRight(speed);
+  forwardLeft(speed);
+}
+
+void turnLeft(byte speed) {
+  stopped = false;
+  backwardLeft(speed);
+  forwardRight(speed);
+}
+
+double * getDistances() {
+  double * distances = (double *) malloc(3 * sizeof(double));
+  distances[0] = getDistance(LEFT);
+  distances[1] = getDistance(CENTER);
+  distances[2] = getDistance(RIGHT);
+  return distances;
+}
+
+double getDistance(DISTANCE_SENSOR sensor) {
+  unsigned long duration;
+  int trig;
+  int echo;
+  switch (sensor) {
+    case RIGHT:
+      trig = DISTANCE_SENSOR_1_TRIG;
+      echo = DISTANCE_SENSOR_1_ECHO;
+      break;
+    case CENTER:
+      trig = DISTANCE_SENSOR_2_TRIG;
+      echo = DISTANCE_SENSOR_2_ECHO;
+      break;
+    case LEFT:
+      trig = DISTANCE_SENSOR_3_TRIG;
+      echo = DISTANCE_SENSOR_3_ECHO;
+      break;
+  }
+  digitalWrite(trig, HIGH);// Generate
+  delayMicroseconds(10);   // the
+  digitalWrite(trig, LOW); // pulse
+  duration = pulseIn(echo, HIGH, MAX_WAIT_TIME);
+  return (duration/1e6) * getSoundSpeed(DHTSensor.temperature) / 2;
+}
+
+double getSoundSpeed(double temperature) {
+  return SOUND_SPEED_FACTOR * temperature + SOUND_SPEED_CONSTANT;
+}
+
+double averageDistance() {
+  double * distances = getDistances();
+  int corrects = 0;
+  double sum = 0;
+  for (size_t i = 0; i < 3; i++) {
+    if (distances[i] > 0) {
+      corrects++;
+      sum += distances[i];
+    }
+  }
+  if (corrects > 0) return sum / corrects;
+  else return 0;
+}
+
+unsigned int argmin(double * array, unsigned int length) {
+  unsigned int minIndex = 0;
+  for (size_t i = 1; i < length; i++)
+    if (array[i] < array[minIndex]) minIndex = i;
+  return minIndex;
+}
+
+void navigate(byte speed) {
+  double * distances = getDistances();
+  if (distances[CENTER] < MIN_OBSTACLE_DISTANCE) moveBackward(speed);
+  else
+  if (distances[LEFT] > 0 && distances[LEFT] < MAX_OBSTACLE_DISTANCE && argmin(distances, 3) == LEFT) direction = RIGHT;
+  else if (distances[RIGHT] > 0 && distances[RIGHT] < MAX_OBSTACLE_DISTANCE && argmin(distances, 3) == RIGHT) direction = LEFT;
+  else direction = CENTER;
+  #ifdef DEBUG
+  Serial.print(direction == CENTER ? "^" : (direction == RIGHT ? ">" : "<"));
+  #else
+  if (direction == CENTER) moveForward(speed);
+  else if (direction == RIGHT) turnRight(speed);
+  else turnLeft(speed);
+  #endif
+}
---
+++ Code/Arduino/src/CarControl.h
@@ -0,0 +1,69 @@
+#ifndef CARCONTROL_H
+#define CARCONTROL_H
+
+#include <Arduino.h>
+#include <dht.h>
+
+#define BAUD_RATE 9600
+
+// HR-S04 distance sensors
+#define DISTANCE_SENSOR_1_TRIG 22
+#define DISTANCE_SENSOR_1_ECHO 23
+#define DISTANCE_SENSOR_2_TRIG 24
+#define DISTANCE_SENSOR_2_ECHO 25
+#define DISTANCE_SENSOR_3_TRIG 26
+#define DISTANCE_SENSOR_3_ECHO 27
+
+enum DISTANCE_SENSOR { LEFT = 0, CENTER = 1, RIGHT = 2 };
+
+// DHT22 sensors
+#define DHT_DATA 51
+
+// DC Motors
+#define RIGHT_MOTOR_PIN1 11
+#define RIGHT_MOTOR_PIN2 10
+#define LEFT_MOTOR_PIN1 9
+#define LEFT_MOTOR_PIN2 8
+#define RIGHT_MOTOR_SPEED_PIN 12
+#define LEFT_MOTOR_SPEED_PIN 13
+
+#define BASE_TURN_DELAY 400
+#define FORWARD_DELAY 400
+
+double getSoundSpeed(double);
+double getDistance(DISTANCE_SENSOR sensor);
+double * getDistances();
+double averageDistance();
+void forwardRight(byte);
+void backwardRight(byte);
+void forwardLeft(byte);
+void backwardLeft(byte);
+void stop();
+void moveForward(byte);
+void moveBackward(byte);
+void turnRight(byte);
+void turnLeft(byte);
+void navigate(byte);
+unsigned int argmin(double *, unsigned int);
+
+// Commands
+#define GET_DISTANCES 'D'
+#define MOVE_FORWARD 'F'
+#define MOVE_BACKWARD 'B'
+#define TURN_LEFT 'L'
+#define TURN_RIGHT 'R'
+#define STOP 'S'
+#define TEMP 'T'
+#define SET_SPEED 'P'
+#define NAVIGATE 'N'
+
+// Other
+#define MAX_WAIT_TIME 30000 // Maximum waiting time for the echo (3O ms)
+#define SOUND_SPEED_CONSTANT 331.4
+#define SOUND_SPEED_FACTOR 0.6
+#define MAX_OBSTACLE_DISTANCE 0.5  // meters
+#define MIN_OBSTACLE_DISTANCE 0.2  // meters
+#define SERIAL_TIMEOUT 50 // milliseconds
+#define MOVE_ACTION_TIME 100
+
+#endif
